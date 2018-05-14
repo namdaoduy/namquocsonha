6a305d6d095042a06413b15d2ca501558af6ffaf
Đào Duy Nam <lenkc98@gmail.com>
2018-05-15 02:07:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var point = document.getElementById("point");
 var point_div = document.getElementById("point-div");
 var display_div = document.getElementById("display");
 
-var maxfood = 154;
+var maxfood = 151;
 var count_food;
 var count_point;
 var map;
