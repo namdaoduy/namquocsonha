733d69d29f6f1db463a9f09129cabf71d9dc8213
Đào Duy Nam <lenkc98@gmail.com>
2018-05-14 22:20:03 +0700

    Big Update

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ var point = document.getElementById("point");
 var point_div = document.getElementById("point-div");
 var display_div = document.getElementById("display");
 
-var maxpoint = 298;
-var count;
+var maxfood = 154;
+var count_food;
+var count_point;
 var map;
 var mapH = 15;
 var mapW = 20;
 var brick = "╬╩╦╣╠╝╚╗╔║═";
 var output;
+var super_mode = 0;
 
 
 
@@ -21,15 +23,22 @@ function Pacman() {
 	this.dx,
 	this.dy,
 	this.time,
-	this.speed = 200,
+	this.speed,
+	this.resetAll = function() {
+		this.speed = 200;
+	},
 	this.move = function() {
 		map[this.pos_x][this.pos_y] = ' ';
 		this.pos_x += this.dx;
 		this.pos_y += this.dy;
 		if (map[this.pos_x][this.pos_y] == '◦') {
-			count++;
+			count_food++;
+			count_point += 10;
 		} else if (map[this.pos_x][this.pos_y] == 'Ѽ') {
-			count += 50;
+			count_food++;
+			count_point += 100;
+			blink("blue");
+			bigFruit();
 		}
 		map[this.pos_x][this.pos_y] = '☺';
 		display();
@@ -65,7 +74,15 @@ function Ghost() {
 	this.dy,
 	this.foot,
 	this.time,
-	this.speed = 100,
+	this.speed,
+	this.dead = 0,
+	this.resetAll = function() {
+		this.resetSpeed();
+		this.dead = 0;
+	},
+	this.resetSpeed = function() {
+		this.speed = 200;
+	}
 	this.move = function() {
 		map[this.pos_x][this.pos_y] = this.foot;
 		this.pos_x += this.dx;
@@ -83,10 +100,12 @@ function Ghost() {
 		map[this.pos_x][this.pos_y] = '☻';
 	},
 	this.start = function() {
-		var _this = this;
-		this.time = setInterval(function(){
-			_this.loop();
-		}, _this.speed);
+		if (this.dead == 0) {
+			var _this = this;
+			this.time = setInterval(function(){
+				_this.loop();
+			}, _this.speed);	
+		}
 	},
 	this.loop = function() {
 		if (brick.indexOf(map[this.pos_x + this.dx][this.pos_y + this.dy]) < 0){
@@ -132,6 +151,10 @@ mainGif();
 function start() {
 	resetGame();
 
+	pacman.resetAll();
+	ghost1.resetAll();
+	ghost2.resetAll();
+
 	pacman.spawn(1,1,0,-1);
 	ghost1.spawn(13,8,0,1);
 	ghost2.spawn(13,18,0,-1);
@@ -148,7 +171,8 @@ function start() {
 }
 
 function resetGame() {
-	count = 0;
+	count_food = 0;
+	count_point = 0;
 	map = [
 		['╔','═','═','═','═','═','═','═','═','═','═','═','═','═','═','═','═','═','═','╗'],
 		['║','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','◦','║'],
@@ -185,7 +209,7 @@ function display() {
 		output += '<br>';
 	}	
 	nam.innerHTML = output;
-	point.innerHTML = count;
+	point.innerHTML = count_point;
 }
 
 function check() {
@@ -205,16 +229,34 @@ function check() {
 		}
 	}
 	if (checkPos(pacman, ghost1) || checkPos(pacman, ghost2)) {
-		clearTime();
-		map[pacman.pos_x][pacman.pos_y] = '۩';
-		display();
-		blink('red');
-		setTimeout(function() {
-			nam.innerHTML = '\n\n\n\n\n      <strong>GAME OVER</strong>\n\n' +
-							'     Press START\n\n\n';
-		}, 1000);
+		if (super_mode == 1) {
+			map[pacman.pos_x][pacman.pos_y] = '☺';
+			count_point += 500;
+			if (checkPos(pacman, ghost1)) {
+				clearInterval(ghost1.time);
+				ghost1.dead = 1;
+				ghost1.pos_x = NaN;
+				ghost1.pos_y = NaN;
+			} else {
+				clearInterval(ghost2.time);
+				ghost2.dead = 1;
+				ghost2.pos_x = NaN;
+				ghost2.pos_y = NaN;
+			}
+		}
+		else {
+			clearTime();
+			map[pacman.pos_x][pacman.pos_y] = '۩';
+			display();
+			blink('red');
+			setTimeout(function() {
+				nam.innerHTML = '\n\n\n\n\n      <strong>GAME OVER</strong>\n\n' +
+								'     Press START\n\n\n';
+			}, 1000);
+		}
+		
 	}
-	if (count == maxpoint) {
+	if (count_food == maxfood) {
 		clearTime();
 		blink('blue');
 		setTimeout(function() {
@@ -305,11 +347,23 @@ function help() {
 
 function blink(color) {
 	var i = 0;
-	var time = setInterval(changeClass, 100, 5);
+	var time = setInterval(changeClass, 100);
 	function changeClass() {
 		display_div.classList.toggle(color);
 		i++;
 		if (i == 8) {
+			clearInterval(time);
+		}
+	}
+}
+
+function change(color) {
+	var j = 0;
+	var time = setInterval(changeClass, 100);
+	function changeClass() {
+		display_div.classList.toggle(color);
+		j++;
+		if (j == 3) {
 			clearInterval(time);
 		}
 	}
@@ -371,6 +425,49 @@ function mainGif() {
 }
 
 
-function boost() {
+function bigFruit() {
+	var rand = Math.floor((Math.random() * 4) + 1);
+	switch (rand) {
+		case 1:
+			boostSpeed(ghost1, 4);
+			break;
+		case 2:
+			boostSpeed(pacman, 2);
+			break;
+		case 3:
+			boostSpeed(pacman, 0.5);
+			break;
+		case 4:
+			superPacman();
+			break;
+	}
+}
 
+function boostSpeed(target, multi) {
+	target.speed = Math.floor(target.speed / multi);
+	clearInterval(target.time);
+	target.start();
+}
+
+function superPacman() {
+	clearTime();
+	change("gold");
+	pacman.speed = 150;
+	ghost1.speed = 800;
+	ghost2.speed = 800;
+	super_mode = 1;
+	pacman.start();
+	ghost1.start();
+	ghost2.start();
+	setTimeout(function() {
+		change("gold");
+		clearTime();
+		super_mode = 0;
+		pacman.resetAll();
+		ghost1.resetSpeed();
+		ghost2.resetSpeed();
+		pacman.start();
+		ghost1.start();
+		ghost2.start();
+	}, 5000);
 }
